fd0c99162d4020bd451e9776c2759efadea5d9b1
Chris <117781333+chrism-la@users.noreply.github.com>
2024-07-21 18:16:45 -0700

    Created SideBarIcon component and imported icons from React Icon package

--- discord-inspired-dashboard/src/SideBar.jsx
+++ discord-inspired-dashboard/src/SideBar.jsx
@@ -1,17 +1,15 @@
+import { FaSafari, FaPoo, FaFire, FaCamera, FaTwitter } from 'react-icons/fa';
+
 export default function SideBar() {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white shadow-lg">
-            <SideBarIcon icon={}/>
-            <SideBarIcon icon={}/>
-            <SideBarIcon icon={}/>
-            <SideBarIcon icon={}/>
-            <SideBarIcon icon={}/>
+            <SideBarIcon icon={<FaPoo size="28" />} />
+            <SideBarIcon icon={<FaSafari size="28" />} />
+            <SideBarIcon icon={<FaFire size="28" />} />
+            <SideBarIcon icon={<FaCamera size="28" />} />
+            <SideBarIcon icon={<FaTwitter size="28" />} />
         </div>
     );
 }
 
-const SideBarIcon = ({icon}) => (
-    <div className="sidebar-icon">
-        {icon}
-    </div>
-)
+const SideBarIcon = ({ icon }) => <div className="sidebar-icon">{icon}</div>;
