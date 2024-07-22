5da59ff461ba38b9a6d5cc9a5686a938fef4981c
Chris <117781333+chrism-la@users.noreply.github.com>
2024-07-21 18:11:19 -0700

    installed React Icons Package

--- discord-inspired-dashboard/src/SideBar.jsx
+++ discord-inspired-dashboard/src/SideBar.jsx
@@ -1,11 +1,17 @@
 export default function SideBar() {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-gray-900 text-white shadow-lg">
-            <i>A</i>
-            <i>B</i>
-            <i>C</i>
-            <i>D</i>
-            <i>E</i>
+            <SideBarIcon icon={}/>
+            <SideBarIcon icon={}/>
+            <SideBarIcon icon={}/>
+            <SideBarIcon icon={}/>
+            <SideBarIcon icon={}/>
         </div>
     );
 }
+
+const SideBarIcon = ({icon}) => (
+    <div className="sidebar-icon">
+        {icon}
+    </div>
+)
